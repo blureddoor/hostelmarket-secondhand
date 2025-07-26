a4576752bb28ca5762c14dae63edf1ba21151e03
Leo Vintila <lvintila@iMac-de-Leo.local>
2025-07-26 12:11:08 +0200

    Initial Spring Boot project structure

---
+++ src/test/java/com/hostelmarket/secondhand/HostelmarktetSecondhandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hostelmarket.secondhand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelmarktetSecondhandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
